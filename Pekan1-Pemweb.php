6ce90dc5e61501bfb5abb96db483e342028a9a2d
Febriansyah2828 <zombie57864@gmail.com>
2025-04-12 12:01:34 +0700

    Add files via upload

---
+++ Pekan1-Pemweb.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Nyoba PHP</title>
+</head>
+<body>
+    <h3>Welcome to php </h3>
+    <?php
+    <hr>
+      $_nama = "Ariel Nurrizky Hidayat"
+      $_umur = 19;
+      $_berat = 55;
+      $_prodi = "sistem informasi"
+      $_kampus = "STT NURUL FIKRI"
+    ?>
+    </hr> 
+    <?php
+    // Menampikan nilai variabel
+      echo "Nama saya $_nama, Umur saya $_umur tahun, ";
+      echo "<br> denganberat badan $_berat kg";
+      echo "<br> Saya sedang menempuh pendidikan di prodi $_prodi";
+      echo "<br> Di $_kampus"
+    
+</body>
+</html>
